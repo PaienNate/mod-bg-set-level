f3885f996c7f19498a95c36165ebeee1aa2ab45e
Alvin Zhu <alvin.zhuge@gmail.com>
2023-01-23 12:54:00 +0800

    fix

--- sql/characters/base/custom_bg_level.sql
+++ sql/characters/base/custom_bg_level.sql
@@ -9,8 +9,3 @@ CREATE TABLE IF NOT EXISTS `custom_bg_level` (
 -- Dumping data for table acore_characters.custom_bg_level: ~0 rows (approximately)
 /*!40000 ALTER TABLE `custom_bg_level` DISABLE KEYS */;
 /*!40000 ALTER TABLE `custom_bg_level` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
